10b22563a25cb75f3e0542e1627e4418a8d6fd8a
sma342 <gruby.2002@o2.pl>
2017-11-27 21:54:26 +0100

    Initial commit

---
+++ Source/Test/Test/CompProperties_SecondLayer.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Maciej
+ * Date: 02.02.2017
+ * Time: 14:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Verse;
+
+namespace Test
+{
+	internal class CompProperties_SecondLayer : CompProperties
+	{
+		public GraphicData graphicData;
+
+		public AltitudeLayer altitudeLayer;
+
+		public float Altitude
+		{
+			get
+			{
+				return Altitudes.AltitudeFor(this.altitudeLayer);
+			}
+		}
+
+		public CompProperties_SecondLayer()
+		{
+			this.compClass = typeof(CompSecondLayer);
+		}
+	}
+}
---
+++ Source/Test/Test/CompSecondLayer.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Maciej
+ * Date: 02.02.2017
+ * Time: 14:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using Verse;
+
+namespace Test
+{
+	internal class CompSecondLayer : ThingComp
+	{
+		private Graphic graphicInt;
+
+		public CompProperties_SecondLayer Props
+		{
+			get
+			{
+				return (CompProperties_SecondLayer)this.props;
+			}
+		}
+
+		public virtual Graphic Graphic
+		{
+			get
+			{
+				bool flag = this.graphicInt == null;
+				Graphic badGraphic;
+				if (flag)
+				{
+					bool flag2 = this.Props.graphicData == null;
+					if (flag2)
+					{
+						Log.ErrorOnce(this.parent.def + " has no SecondLayer graphicData but we are trying to access it.", 764532);
+						badGraphic = BaseContent.BadGraphic;
+						return badGraphic;
+					}
+					this.graphicInt = this.Props.graphicData.GraphicColoredFor(this.parent);
+				}
+				badGraphic = this.graphicInt;
+				return badGraphic;
+			}
+		}
+
+		public override void PostDraw()
+		{
+			base.PostDraw();
+			this.Graphic.Draw(Gen.TrueCenter(this.parent.Position, this.parent.Rotation, this.parent.def.size, this.Props.Altitude), this.parent.Rotation, this.parent);
+		}
+	}
+}
